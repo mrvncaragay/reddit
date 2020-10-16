699dab8c5ffbd266f198072ece43294b6895b83b
Marv <mrvncaragay@gmail.com>
2020-10-16 15:16:58 -0700

    Interface register page to grapql server and resolve cors error

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,16 +3,30 @@ import { Formik, Form } from 'formik';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
 import { Box, Button } from '@chakra-ui/core';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
+
+  const [, register] = useRegisterMutation();
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          console.log(response);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // registration work
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
